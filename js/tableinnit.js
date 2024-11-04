0e15185f510d327eaf49aeb615738755d8d61733
Hovanyecz István Dániel <94844047+HovanyeczIstvanDaniel84@users.noreply.github.com>
2024-11-04 12:05:07 +0100

    final version

--- js/tableinnit.js
+++ js/tableinnit.js
@@ -1,13 +1,16 @@
 
-for (var i = 0; i < 24; i++) {
+function initTable() {
+    for (var i = 0; i < 24; i++) {
 
-    let tr = document.createElement("tr");
-    let th = document.createElement("th");
-    th.setAttribute("scope", "row");
-    th.innerHTML = `${i}:00`;
-    tr.appendChild(th)
-    for (var j = 0; j < 7; j++) {
-        tr.innerHTML += `<td hourIndex="${i}" class="text-center"></td>`;
+        let tr = document.createElement("tr");
+        let th = document.createElement("th");
+        th.setAttribute("scope", "row");
+        th.innerHTML = `${i}:00`;
+        tr.appendChild(th)
+        for (var j = 0; j < 7; j++) {
+            tr.innerHTML += `<td hourIndex="${i}" dayIndex="${j}" class="text-center"></td>`;
+        }
+        document.querySelector("tbody").appendChild(tr);
     }
-    document.querySelector("tbody").appendChild(tr);
+
 }
