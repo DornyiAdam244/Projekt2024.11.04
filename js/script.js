e000896f6c1e6b147befaf1b79c5552c3da3bae0
Hovanyecz István Dániel <94844047+HovanyeczIstvanDaniel84@users.noreply.github.com>
2024-11-04 10:31:53 +0100

    event handle fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const cols = document.querySelectorAll("td")
 cols.forEach(col =>
     col.addEventListener("click", () => {
         let event = prompt("Adj meg egy eseményt (vagy hagyd üresen a törléshez):");
-        col.innerHTML = event;
+        if (event != null) {
+            col.innerHTML = `<span>${event}</span>`;
+        }
 }));
 
 function resetTable() {
