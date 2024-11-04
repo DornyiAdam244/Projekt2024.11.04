d16c5a1764d3b99112d2ce8e3e9d85d47a687ccc
Dornyi Adam Robert <94841029+DornyiAdam244@users.noreply.github.com>
2024-11-04 10:42:58 +0100

    asd

--- js/tableinnit.js
+++ js/tableinnit.js
@@ -1,14 +1,8 @@
-
+let td = `<td class="text-center"></td>`
 for (var i = 0; i < 24; i++) {
     document.querySelector("tbody").innerHTML +=
     `<tr>
     <th scope="row">${i}:00</th>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
+    
     </tr>`
 }
